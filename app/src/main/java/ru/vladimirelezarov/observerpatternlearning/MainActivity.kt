574f13dfa5b0c27fa9b62be90472fd15d91c0730
Владимир Елезаров <wavilla23@gmail.com>
2020-11-17 12:26:47 +0300

    Add some dependencies just for creating a template. Replace multiple calls votesCounter with apply fun.

--- app/src/main/java/ru/vladimirelezarov/observerpatternlearning/MainActivity.kt
+++ app/src/main/java/ru/vladimirelezarov/observerpatternlearning/MainActivity.kt
@@ -13,16 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         votesCounter = VotesCounter()
-        votesCounter.addElector(GuyForTrump("John", "Sidorov"))
-        votesCounter.addElector(GuyForTrump("Harry", "Potter"))
-        votesCounter.addElector(GuyForTrump("Volk", "Tambovsky"))
-        votesCounter.addElector(GuyForTrump("John", "Snow"))
-        votesCounter.addElector(GuyForBiden("Pablo", "Escobar"))
-        votesCounter.addElector(GuyForBiden("Alphonso", "Capone"))
-        votesCounter.addElector(GuyForBiden("Neo", "Matrix"))
-        votesCounter.addElector(GuyForBiden("Jesus", "Christ"))
-        votesCounter.addElector(AverageElector("Ivan", "Ivanov"))
-        votesCounter.addElector(AverageElector("Petr", "Petrov"))
+        votesCounter.apply {
+            addElector(GuyForTrump("John", "Sidorov"))
+            addElector(GuyForTrump("Harry", "Potter"))
+            addElector(GuyForTrump("Volk", "Tambovsky"))
+            addElector(GuyForTrump("John", "Snow"))
+            addElector(GuyForBiden("Pablo", "Escobar"))
+            addElector(GuyForBiden("Alphonso", "Capone"))
+            addElector(GuyForBiden("Neo", "Matrix"))
+            addElector(GuyForBiden("Jesus", "Christ"))
+            addElector(AverageElector("Ivan", "Ivanov"))
+            addElector(AverageElector("Petr", "Petrov"))
+        }
 
         val simpleTV = findViewById<TextView>(R.id.simple_tv)
         simpleTV.setOnClickListener {
